e203fbada701b68e0a7f0d27dd11c657405bc953
kwon <dohun_dev@naver.com>
2025-01-23 15:06:55 +0900

    feat: Curriculum section common component # title, content 입력

---
+++ web/src/component/Curriculum-Text-Box.jsx
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+export default function CurriculumInfo({ title, content }) {
+  return (
+    <Wrapper>
+      <Title>{title}</Title>
+      <Content dangerouslySetInnerHTML={{ __html: content }} />
+    </Wrapper>
+  );
+}
+const Wrapper = styled.div`
+  padding: 20px 30px;
+`;
+const Title = styled.div`
+  font-family: "Pretendard-bold";
+  font-size: 20px;
+  margin-bottom: 5px;
+  color: black;
+  @media (min-width: 980px) {
+    font-size: 32px;
+  }
+`;
+
+const Content = styled.div`
+  font-size: 15px;
+  line-height: 2;
+  color: #888888;
+  @media (min-width: 980px) {
+    font-size: 18px;
+  }
+`;
