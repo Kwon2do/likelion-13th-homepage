6db642e5fc85ef22a6e53702e5a8583819fb44c6
kwon <dohun_dev@naver.com>
2025-02-10 05:56:09 +0900

    fix server error2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://likelion-inu.com', 'http://localhost:3000'],
+    origin: [/https:\/\/.*likelion-inu\.com$/, 'http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
